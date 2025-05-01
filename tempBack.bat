e2afa23fca56ab0c1da0f37dfae26dbb533ef321
akshat <aparwani031@gmail.com>
2025-05-01 16:17:08 +0530

    mysql updated to current verision

--- tempBack.bat
+++ tempBack.bat
@@ -1,2 +1,2 @@
 @echo off
-mysqldump -u root -ptv%%f*^! sample > "C:\Users\Aparw\ShreeGurukripaJewellers\src\resources\23_04_25___16_31_21_backup.sql"
+mysqldump -u root -ptv%%f*^! sample > "C:\Users\Aparw\ShreeGurukripaJewellers\src\resources\01_05_25___16_16_28_backup.sql"
