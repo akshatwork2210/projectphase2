257259758ddb4db76e28960fa197d890a20f5761
akshat <aparwani031@gmail.com>
2025-05-21 16:22:51 +0530

    production

--- tempBack.bat
+++ tempBack.bat
@@ -1,2 +1,2 @@
 @echo off
-mysqldump -u root -ptv%%f*^! sample > "C:\Users\Aparw\ShreeGurukripaJewellers\src\resources\19_05_25___20_57_49_backup.sql"
+mysqldump -u root -ptv%%f*^! sample > "C:\Users\Aparw\ShreeGurukripaJewellers\src\resources\21_05_25___16_22_29_backup.sql"
