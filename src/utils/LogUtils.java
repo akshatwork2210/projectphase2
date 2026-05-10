74c3ec952ebda69d9198f753c7c15692054b8a7b
Akshat <aparwani031@gmail.com>
2026-05-10 14:27:21 +0530

    minor error solved

--- src/utils/LogUtils.java
+++ src/utils/LogUtils.java
@@ -3,6 +3,6 @@ package utils;
 public class LogUtils {
     //----------------------------------orderslips logigin class-------w----------------------------------------
     public void generateInventorySnapShot(){
-        try()
+        
     }
 }
